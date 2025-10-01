2000ec9e8b2e2aa44bc7127d608c4dfc14a3baaa
zcriticz <zcriticz.dev@gmail.com>
2025-09-30 21:00:21 -0300

    feat: add JsonParser class for JSON serialization and deserialization

---
+++ src/com/github/zcriticz/ziplocator/util/JsonParser.java
@@ -0,0 +1,17 @@
+package com.github.zcriticz.ziplocator.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonParser {
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public static String toJson(Object obj) {
+        return gson.toJson(obj);
+    }
+
+    public static <T> T fromJson(String json, Class<T> classOfT) {
+        return gson.fromJson(json, classOfT);
+    }
+
+}
